4a6cd6e9cff9bbfe9b9c9be69ddeea7d3162bc03
Ray Mercedes <contact@RayMercedes.com>
2018-10-28 04:46:36 -0400

    Initial commit

---
+++ footer.php
@@ -0,0 +1,39 @@
+<?php
+#############footer info##########
+echo "<style> a {color:white;}</style>";
+$footer=array("'http://start.mobilerepairtechs.com'>RepairShopr",
+              "'https://cpr.repairq.io/'>RepairQ" ,
+              "'http://www.cellphonerepair.com/gainesville-fl'>Website Front End" ,
+              "'https://www.cellphonerepair.com/gainesville-fl/login/' >Website Back End",
+              "'http://facebook.com/mobilerepairtechs'>Facebook",
+              "'http://localhost/randomize-duties/'>Tasks",
+              "'http://localhost/marketing-cards/'>Marketing Cards",
+              "'http://localhost/craigslist-ads/'>Craigslist Ad's",
+              "'http://localhost/MRT-Miami/'>MRT-Miami"
+
+
+             );
+
+sort($footer);
+
+
+echo "<div style='height:20px;color:white;
+      background-color:#222;bottom:0;text-align:center;
+      width:100%;position:absolute;>";
+
+echo "
+        <table style='width:100%';color:white;>
+           <tr style='align-text:center;width:100%;color:white;'>";
+
+
+echo "<div style='color:white';>";
+
+
+echo "| ";
+foreach($footer as $footer) {
+                                echo "<td style='text-decoration-color:white;'> <a  href=$footer </a> | </td>";   
+                            }
+echo "</div>";
+
+echo "</div>";
+?>
